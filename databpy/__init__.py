867b4ecf43013373392ee84e7dd27c62bef770ef
Brady Johnston <brady.johnston@me.com>
2024-12-19 08:37:30 +0800

    fix docs building

--- databpy/__init__.py
+++ databpy/__init__.py
@@ -10,6 +10,7 @@ from .vdb import import_vdb
 import bpy
 from .addon import register, unregister
 from .utils import centre, lerp
+from .collection import create_collection
 from .attribute import (
     named_attribute,
     store_named_attribute,
