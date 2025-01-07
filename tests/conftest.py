8ac7e90a77d7aeccb7fb74acde20fa4d98f53faf
Brady Johnston <brady.johnston@me.com>
2025-01-07 15:55:30 +0800

    Update conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,21 +1,21 @@
-import bpy
-import pytest
-import databpy
+# import bpy
+# import pytest
+# import databpy
 
-databpy.register()
+# databpy.register()
 
 
-@pytest.fixture(autouse=True)
-def run_around_tests():
-    # Code that will run before each tests
+# @pytest.fixture(autouse=True)
+# def run_around_tests():
+#     # Code that will run before each tests
 
-    bpy.ops.wm.read_homefile(app_template="")
-    for tree in bpy.data.node_groups:
-        bpy.data.node_groups.remove(tree)
+#     bpy.ops.wm.read_homefile(app_template="")
+#     for tree in bpy.data.node_groups:
+#         bpy.data.node_groups.remove(tree)
 
-    yield
+#     yield
 
-    bpy.ops.wm.read_homefile(app_template="")
-    # Code that will run after your test, for example:
-    # files_after = # ... do something to check the existing files
-    # assert files_before == files_after
+#     bpy.ops.wm.read_homefile(app_template="")
+#     # Code that will run after your test, for example:
+#     # files_after = # ... do something to check the existing files
+#     # assert files_before == files_after
